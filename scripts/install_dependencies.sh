f8c2d4aaf8edd800ebaeb24ddb8d9ac4c55d1158
Gustavo Lopes <gustavo.hlopess@gmail.com>
2022-12-06 01:44:51 +0000

    also stop old processes before install

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -2,3 +2,14 @@
 # for AWS Linux
 sudo yum install golang -y
 
+# Get the PID of go running process
+pid=$(pgrep go)
+
+# Check if the process is running
+if [ -n "$pid" ]; then
+  # Kill the process
+  kill $pid
+else
+  echo "Process not found"
+fi
+
