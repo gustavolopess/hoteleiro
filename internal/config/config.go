f410853230b1f5ca982e4ac4cb6ca1d4e97a15db
Gustavo Lopes <gustavo.hlopess@gmail.com>
2022-12-05 22:00:15 -0300

    Change aws region to us-east-2

--- internal/config/config.go
+++ internal/config/config.go
@@ -1,7 +1,7 @@
 package config
 
 const (
-	AwsRegion                   = "us-east-1"
+	AwsRegion                   = "us-east-2"
 	TelegramBotToken            = "5627586393:AAHHTc0W5Fjy-dC1CLejshG3ZbJK4va5--E"
 	GoogleSheetId               = "1lfWxf_Wj5IjKjPlu6V2k519Y_RVJh1UU2pDL9VuFxCo"
 	GoogleSheetsCredentialsInS3 = "credentials.json"
