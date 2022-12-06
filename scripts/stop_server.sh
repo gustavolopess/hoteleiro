68b61de8eaa77eca3f4a1953f14d885e105f4d58
Gustavo Lopes <gustavo.hlopess@gmail.com>
2022-12-06 01:42:25 +0000

    kill running process before start a new one

--- scripts/stop_server.sh
+++ scripts/stop_server.sh
@@ -1,1 +1,12 @@
 #!/bin/bash
+
+# Get the PID of the process
+pid=$(pgrep go)
+
+# Check if the process is running
+if [ -n "$pid" ]; then
+  # Kill the process
+  kill $pid
+else
+  echo "Process not found"
+fi
